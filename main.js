8eaffb79db25a8f2e2deb351d2f68cbbb836b2cd
Chris Boesch <ChrisBoesch@nus.edu.sg>
2019-09-14 15:28:19 +0800

    Added remove button

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ var app = new Vue({
   el: "#app",
   data: {
     title: "Todo list:",
-    items: ["Apple", "Banana", "Carrot"]
+    items: ["Apple", "Banana", "Carrot"],
+    nextItem: "Danish"
+  },
+  methods: {
+    removeItem: function(item) {
+      let index = this.items.indexOf(item);
+      this.items.splice(index, 1);
+    }
   }
 });
